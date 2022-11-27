293f0c46a2638829fd75b21e5a90dc61523362cb
v88NoahJerreel <nguillen@village88.com>
2022-11-27 19:50:38 +0800

    added migration file

--- web-backend/server/config/migrations/2022-10-initial-tables.migration.sql
+++ web-backend/server/config/migrations/2022-10-initial-tables.migration.sql
@@ -9,4 +9,26 @@ CREATE TABLE `the_wall`.`users` (
   `updated_at` DATETIME NULL,
   PRIMARY KEY (`id`),
   INDEX `index_users_on_email_address` (`email_address` ASC) VISIBLE);
-\
+
+
+CREATE TABLE `the_wall`.`posts` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `content` TEXT NULL,
+  `created_at` VARCHAR(45) NULL,
+  `updated_at` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`),
+  INDEX `indx_users_on_post` (`user_id` ASC) VISIBLE);
+
+
+CREATE TABLE `the_wall`.`comments` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `post_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
+  `content` TEXT NULL,
+  `created_at` DATETIME NULL,
+  `updated_at` DATETIME NULL,
+  PRIMARY KEY (`id`),
+  INDEX `indx_post_on_comments` (`post_id` ASC) VISIBLE,
+  INDEX `indx_user_on_comments` (`user_id` ASC) VISIBLE,
+  INDEX `indx_post_user_on_comments` (`post_id` ASC, `user_id` ASC) VISIBLE);
